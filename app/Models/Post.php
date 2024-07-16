cb26fef1c3971f6a38fc45a9b58fd732a98b5621
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-16 13:45:03 +0700

    improve: cast id as string because type is uuid and auto assign when create

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use App\Enums\Status;
 
 class Post extends Model
 {
     use HasFactory;
 
+    // Mapping columns to their respective data types
     protected $casts = [
         'tags' => 'array',
+        'id' => 'string', // This will cast the id to a string when retrieved
     ];
 
     protected $fillable = [
@@ -24,4 +27,14 @@ class Post extends Model
     protected $attributes = [
         'status' => Status::ACTIVE->value,
     ];
+
+    public static function boot ()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            // Generate a UUID for the post ID
+            $post->id = STR::uuid();
+        });
+    }
 }
