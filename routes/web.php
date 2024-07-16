8e734104542029090251b8904c3b1a5100eb13a8
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-16 12:35:45 +0700

    init: define crud route for post module

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Define the resource route for the PostController, which will create the following routes:
+ * GET /posts
+ * GET /posts/create
+ * POST /posts
+ * GET /posts/{post}
+ * GET /posts/{post}/edit
+ * PUT/PATCH /posts/{post}
+ * DELETE /posts/{post}
+ */
+Route::resource('posts', PostController::class);
