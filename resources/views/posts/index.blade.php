a5c327fc12288824cf301178d58a48f5234eb7a0
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-16 12:35:54 +0700

    init: define page for index post

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Postingan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">Index Post</h3>
+                    <hr>
+                </div>
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-3">ADD POST</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">TITLE</th>
+                                    <th scope="col">STATUS</th>
+                                    <th scope="col" style="width: 20%">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- Check Data if available -->
+                                @forelse ($posts as $post)
+                                    <tr>
+                                        <td>{{ $product->title }}</td>
+                                        <td>{{ $product->status }}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                <!-- If Data Not Available -->
+                                @empty
+                                    <div class="alert alert-danger">
+                                        Data Post belum Tersedia.
+                                    </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        <!-- Pagination -->
+                        {{ $posts->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
