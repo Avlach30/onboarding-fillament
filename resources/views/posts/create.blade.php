63763d98b52fd7e2450878ff318460eb7dae4a36
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-16 14:00:22 +0700

    init: detail page of single post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,7 +8,7 @@ use App\Enums\Status;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add New Products - SantriKoding.com</title>
+    <title>Add New Post</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body style="background: lightgray">
