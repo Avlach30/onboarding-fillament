5773149e255a0b1bd415b719fe62dc2c582b270f
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-16 14:36:54 +0700

    improve: add alert if success or fail

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -59,5 +59,31 @@
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- get message with sweetalert from action create, edit, and delete -->
+    <script>
+        //message with sweetalert
+        @if(session('success'))
+            Swal.fire({
+                icon: "success",
+                title: "BERHASIL",
+                text: "{{ session('success') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        @elseif(session('error'))
+            Swal.fire({
+                icon: "error",
+                title: "GAGAL!",
+                text: "{{ session('error') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        @endif
+
+    </script>
 </body>
 </html>
