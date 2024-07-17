7cb862795fe0dbbde617298c85002095b36d0c7e
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-17 11:56:13 +0700

    init: page update profile with custom page

--- resources/views/filament/pages/app/profile.blade.php
+++ resources/views/filament/pages/app/profile.blade.php
@@ -1,3 +1,8 @@
 <x-filament-panels::page>
-
+    <x-filament::grid @class(['gap-6']) xl="2">
+        <x-filament::grid.column>
+            <!-- Profile -->
+            <livewire:app.user.profile /> 
+        </x-filament::grid.column>
+    </x-filament::grid>
 </x-filament-panels::page>
