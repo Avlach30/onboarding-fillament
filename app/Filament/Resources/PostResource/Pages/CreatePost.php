72ef2574a0b1f8b820cea36ec2054a63e6ea028d
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-19 11:29:06 +0700

    improve: add creator id from logged user at create post

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreatePost extends CreateRecord
 {   
@@ -19,8 +20,11 @@ class CreatePost extends CreateRecord
     // Function for mutating the form data before saving it
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        $loggedUserId = auth()->user()->id;
+
         // Split the tags string into an array if it exists, otherwise return a null value
         $data['tags'] = $data['tags'] ? explode(',', $data['tags']) : null;
+        $data['creator_id'] = $loggedUserId;
 
         return $data;
     }
