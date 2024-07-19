3a3c2c021ccde4caef6837863e79f3f6674dda30
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-19 10:08:47 +0700

    improve: edit user with role

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -10,10 +10,29 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        return array_merge($data, [
+            'role' => $data['role_id'],
+        ]);
+    }
+
     protected function getHeaderActions(): array
     {
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        return array_merge($data, [
+            'role_id' => $data['role'],
+        ]);
+    }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return UserResource::getUrl('index');
     }
 }
