3902accbd866e430bcc660baf1576688db0b1c31
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-22 11:23:58 +0700

    improve: takeout post controller

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-/**
- * Define the resource route for the PostController, which will create the following routes:
- * GET /posts
- * GET /posts/create
- * POST /posts
- * GET /posts/{post}
- * GET /posts/{post}/edit
- * PUT/PATCH /posts/{post}
- * DELETE /posts/{post}
- */
-Route::resource('posts', PostController::class);
